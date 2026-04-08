e843d93aeb2d94b4ed72a60eefa97bc38f32c3e3
ashwinpalve <ashwinpalve01@gmail.com>
2026-04-08 16:12:01 +0530

    chore: remove core folder , old framework replaced with new modules

--- kaft/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from kaft.core.manifold import build_manifold, Manifold
-from kaft.core.metric import FisherRaoMetric
-from kaft.core.topology import KDensity, JordanBoundary
-
-__all__ = ["build_manifold", "Manifold", "FisherRaoMetric", "KDensity", "JordanBoundary"]
